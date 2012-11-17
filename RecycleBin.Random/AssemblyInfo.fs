61fc8fec823e51dbe0614d1691e7378b2b6a40a8
kos59125 <mail@recyclebin.jp>
2012-11-17 13:07:44 +0900

    Added AssermblyInfo.fs

---
+++ RecycleBin.Random/AssemblyInfo.fs
@@ -0,0 +1,19 @@
+﻿namespace RecycleBin.Random
+
+open System.Reflection
+open System.Runtime.CompilerServices
+open System.Runtime.InteropServices
+
+[<assembly: AssemblyTitle("RecycleBin.Random")>]
+[<assembly: AssemblyDescription("Random number generating framework designed for F#.")>]
+[<assembly: AssemblyConfiguration("")>]
+[<assembly: AssemblyCompany("RecycleBin")>]
+[<assembly: AssemblyProduct("RecycleBin.Random")>]
+[<assembly: AssemblyCopyright("Copyright © RecycleBin 2012")>]
+[<assembly: AssemblyTrademark("")>]
+[<assembly: AssemblyCulture("")>]
+
+[<assembly: AssemblyVersion("1.0.0.0")>]
+[<assembly: AssemblyFileVersion("1.0.0.0")>]
+
+()
