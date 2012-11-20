db81cc92523ef9718fb2173b318ecdb34f68d476
kos59125 <mail@recyclebin.jp>
2012-11-20 15:33:36 +0900

    Implemented methods (Combine, Zero, Delay, While, For) used in computation expressions to support for loops (#4).

--- RecycleBin.Random/StateMonad.fs
+++ RecycleBin.Random/StateMonad.fs
@@ -11,6 +11,18 @@ let inline (|>>) m f = bindState m f
 let inline (&>>) m b = bindState m (fun _ -> b)
 
 type StateBuilder () =
-   member this.Bind (m, f) = bindState m f
+   member this.Bind (m, f) = m |>> f
+   member this.Combine (a, b) = a &>> b
    member this.Return (x) = returnState x
    member this.ReturnFrom (m : State<'s, 'a>) = m
+   member this.Zero () = fun x -> (), x
+   member this.Delay (f) = returnState () |>> f
+   member this.While (condition, m) =
+      if condition ()
+      then
+         m |>> (fun () -> this.While (condition, m))
+      else
+         this.Zero ()
+   member this.For (source : seq<'a>, f) =
+      use e = source.GetEnumerator ()
+      this.While (e.MoveNext, this.Delay (fun () -> f e.Current))
--- RecycleBin.Random/StateMonad.fsi
+++ RecycleBin.Random/StateMonad.fsi
@@ -12,5 +12,10 @@ val setState : state:'s -> State<'s, unit>
 type StateBuilder =
    new : unit -> StateBuilder
    member Bind : m:State<'s, 'a> * f:('a -> State<'s, 'b>) -> State<'s, 'b>
+   member Combine : a:State<'s, 'a> * b:State<'s, 'b> -> State<'s, 'b>
    member Return : a:'a -> State<'s, 'a>
    member ReturnFrom : m:State<'s, 'a> -> State<'s, 'a>
+   member Zero : unit -> State<'s, unit>
+   member Delay : (unit -> State<'s, 'a>) -> State<'s, 'a>
+   member While : condition:(unit -> bool) * m:State<'s, unit> -> State<'s, unit>
+   member For : source:seq<'a> * f:('a -> State<'s, unit>) -> State<'s, unit>
