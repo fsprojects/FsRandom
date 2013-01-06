38b2bd32ea23f40cca77841fddaa7869bd72a3a5
kos59125 <mail@recyclebin.jp>
2013-01-07 00:02:03 +0900

    Added a script to edit .nuspec file.

--- tools/XBuildResolver.fsx
+++ tools/XBuildResolver.fsx
@@ -1,51 +1,8 @@
-// Modification of https://gist.github.com/3564010
-
+#load "XmlEditor.fs"
 #r "System.Xml.Linq.dll"
 
-open System
-open System.IO
+open XmlEditor
 open System.Xml.Linq
-open Microsoft.FSharp.Core.LanguagePrimitives
-
-type Trial<'a, 'b> =
-   | Success of 'a
-   | Failure of 'b
-
-type Error =
-   | None = 0
-   | IO = 1
-   | InvalidArgument = 2
-   | InvalidInput = 3
-
-let exit = EnumToValue >> Environment.Exit
-   
-let doEdit edit =
-   function
-      | Success input ->
-         try
-            Success (edit input)
-         with
-            | ex -> Failure (ex, Error.InvalidInput)
-      | Failure (ex, error) ->
-         Failure (ex, error)
-
-let getUserInput () =
-   match fsi.CommandLineArgs with
-      | [| _ |] ->
-         try
-            Success <| Console.In.ReadToEnd ()
-         with
-            | ex -> Failure (ex, Error.IO)
-      | [| _; path |] ->
-         try
-            Success <| File.ReadAllText (path)
-         with
-            | ex -> Failure (ex, Error.IO)
-      | args ->
-         let filename = Path.GetFileName (args.[0])
-         let message = sprintf "Usage: Fsi.exe %s [project.fsproj]" filename
-         let ex = ApplicationException (message) :> exn
-         Failure (ex, Error.InvalidArgument)
 
 let editProject rawProjectXml =
    let project = XDocument.Parse rawProjectXml
@@ -58,13 +15,4 @@ let editProject rawProjectXml =
    |> Seq.iter (fun attribute -> attribute.Remove ())
    project
 
-getUserInput ()
-|> doEdit editProject
-|> function
-   | Success result ->
-      Console.WriteLine (result)
-      Error.None
-   | Failure (ex, error) ->
-      Console.Error.WriteLine (ex.Message)
-      error
-|> exit
+main editProject
