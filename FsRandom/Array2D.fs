12bc28614c02c367e7a52a3d87decafb02fa2884
kos59125 <mail@recyclebin.jp>
2013-09-10 13:49:02 +0900

    Add Array2D module (#38)
    * randomCreate: random version of create.
    * randomInit: random version of init.
    * randomCreateBased: random version of createBased.
    * randomInitBased: random version of initBased.

---
+++ FsRandom/Array2D.fs
@@ -0,0 +1,69 @@
+﻿module FsRandom.Array2D
+
+open System
+
+let randomCreate rowCount columnCount (generator:GeneratorFunction<_, _>) =
+   if rowCount < 0 then
+      outOfRange "rowCount" "`rowCount' must not be negative."
+   elif columnCount < 0 then
+      outOfRange "columnCount" "`columnCount' must not be negative."
+   else
+      fun s0 ->
+         let result = Array2D.zeroCreate rowCount columnCount
+         let mutable s0 = s0
+         for i = 0 to rowCount - 1 do
+            for j = 0 to columnCount - 1 do
+               let r, s' = generator s0
+               result.[i, j] <- r
+               s0 <- s'
+         result, s0
+
+let randomCreateBased rowBase columnBase rowCount columnCount (generator:GeneratorFunction<_, _>) =
+   if rowCount < 0 then
+      outOfRange "rowCount" "`rowCount' must not be negative."
+   elif columnCount < 0 then
+      outOfRange "columnCount" "`columnCount' must not be negative."
+   else
+      fun s0 ->
+         let result = Array2D.zeroCreateBased rowBase columnBase rowCount columnCount
+         let mutable s0 = s0
+         for i = rowBase to rowBase + rowCount - 1 do
+            for j = columnBase to columnBase + columnCount - 1 do
+               let r, s' = generator s0
+               result.[i, j] <- r
+               s0 <- s'
+         result, s0
+
+let randomInit rowCount columnCount (initializer:int -> int -> GeneratorFunction<_, _>) =
+   if rowCount < 0 then
+      outOfRange "rowCount" "`rowCount' must not be negative."
+   elif columnCount < 0 then
+      outOfRange "columnCount" "`columnCount' must not be negative."
+   else
+      fun s0 ->
+         let result = Array2D.zeroCreate rowCount columnCount
+         let mutable s0 = s0
+         for i = 0 to rowCount - 1 do
+            let init = initializer i
+            for j = 0 to columnCount - 1 do
+               let r, s' = init j s0
+               result.[i, j] <- r
+               s0 <- s'
+         result, s0
+
+let randomInitBased rowBase columnBase rowCount columnCount (initializer:int -> int -> GeneratorFunction<_, _>) =
+   if rowCount < 0 then
+      outOfRange "rowCount" "`rowCount' must not be negative."
+   elif columnCount < 0 then
+      outOfRange "columnCount" "`columnCount' must not be negative."
+   else
+      fun s0 ->
+         let result = Array2D.zeroCreateBased rowBase columnBase rowCount columnCount
+         let mutable s0 = s0
+         for i = rowBase to rowBase + rowCount - 1 do
+            let init = initializer i
+            for j = columnBase to columnBase + columnCount - 1 do
+               let r, s' = init j s0
+               result.[i, j] <- r
+               s0 <- s'
+         result, s0
---
+++ FsRandom/Array2D.fsi
@@ -0,0 +1,38 @@
+﻿[<RequireQualifiedAccess>]
+module FsRandom.Array2D
+
+/// <summary>
+/// Creates an array whose elements are randomly generated. 
+/// </summary>
+/// <param name="rowCount">The length of the first dimension of the array.</param>
+/// <param name="columnCount">The length of the second dimension of the array.</param>
+/// <param name="generator">The generator function.</param>
+val randomCreate : rowCount:int -> columnCount:int -> generator:GeneratorFunction<'s, 'a> -> GeneratorFunction<'s, 'a [,]>
+
+/// <summary>
+/// Creates a based array whose elements are randomly generated. 
+/// </summary>
+/// <param name="rowBase">The base of the first dimension of the array.</param>
+/// <param name="columnBase">The base of the second dimension of the array.</param>
+/// <param name="rowCount">The length of the first dimension of the array.</param>
+/// <param name="columnCount">The length of the second dimension of the array.</param>
+/// <param name="generator">The generator function.</param>
+val randomCreateBased : rowBase:int -> columnBase:int -> rowCount:int -> columnCount:int -> generator:GeneratorFunction<'s, 'a> -> GeneratorFunction<'s, 'a [,]>
+
+/// <summary>
+/// Creates an array whose elements are randomly generated. 
+/// </summary>
+/// <param name="rowCount">The length of the first dimension of the array.</param>
+/// <param name="columnCount">The length of the second dimension of the array.</param>
+/// <param name="initializer">The function to take an index and produce a random number generating function.</param>
+val randomInit : rowCount:int -> columnCount:int -> initializer:(int -> int -> GeneratorFunction<'s, 'a>) -> GeneratorFunction<'s, 'a [,]>
+
+/// <summary>
+/// Creates a based array whose elements are randomly generated. 
+/// </summary>
+/// <param name="rowBase">The base of the first dimension of the array.</param>
+/// <param name="columnBase">The base of the second dimension of the array.</param>
+/// <param name="rowCount">The length of the first dimension of the array.</param>
+/// <param name="columnCount">The length of the second dimension of the array.</param>
+/// <param name="generator">The generator function.</param>
+val randomInitBased : rowBase:int -> columnBase:int -> rowCount:int -> columnCount:int -> initializer:(int -> int -> GeneratorFunction<'s, 'a>) -> GeneratorFunction<'s, 'a [,]>
