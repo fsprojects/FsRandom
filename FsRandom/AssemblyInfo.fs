f69d384d5e7762e6e8baf600e34b97f6174de118
kos59125 <mail@recyclebin.jp>
2013-10-05 19:59:03 +0900

    Update version.

--- FsRandom/AssemblyInfo.fs
+++ FsRandom/AssemblyInfo.fs
@@ -13,7 +13,7 @@ open System.Runtime.InteropServices
 [<assembly: AssemblyTrademark("")>]
 [<assembly: AssemblyCulture("")>]
 
-[<assembly: AssemblyVersion("1.0.2.0")>]
-[<assembly: AssemblyInformationalVersion("1.0.2")>]
+[<assembly: AssemblyVersion("1.1.0.0")>]
+[<assembly: AssemblyInformationalVersion("1.1")>]
 
 ()
