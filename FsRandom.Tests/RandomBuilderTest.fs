c45366ee220de1627edb7995d8134e35338e653e
kos59125 <mail@recyclebin.jp>
2013-11-03 06:10:18 +0900

    Add tests for monad laws.

---
+++ FsRandom.Tests/RandomBuilderTest.fs
@@ -0,0 +1,31 @@
+﻿module FsRandom.RandomBuilderTest
+
+open FsUnit
+open NUnit.Framework
+
+[<Test>]
+let ``Satisfies monad law 1 (left identity)`` () =
+   let tester = getDefaultTester ()
+   let a = 1.0
+   let f x = Random.transformBy ((+) x) ``[0, 1)``
+   let l = random.Bind (random.Return (a), f) |> Random.get <| tester
+   let r = f a |> Random.get <| tester
+   r |> should equal l
+
+[<Test>]
+let ``Satisfies monad law 2 (right identity)`` () =
+   let tester = getDefaultTester ()
+   let m = ``[0, 1)``
+   let l = random.Bind (m, random.Return) |> Random.get <| tester
+   let r = m |> Random.get <| tester
+   r |> should equal l
+
+[<Test>]
+let ``Satisfies monad law 3 (associativity)`` () =
+   let tester = getDefaultTester ()
+   let m = ``[0, 1)``
+   let f x = Random.transformBy ((+) x) ``[0, 1)``
+   let g x = Random.transformBy (fun t -> t - x) ``[0, 1)``
+   let l = random.Bind (random.Bind (m, f), g) |> Random.get <| tester
+   let r = random.Bind (m, fun y -> random.Bind (f y, g)) |> Random.get <| tester
+   r |> should equal l
