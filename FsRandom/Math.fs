a204b04fd2fb8b4f23ecadca2430912161ac8237
kos59125 <mail@recyclebin.jp>
2013-11-14 14:29:49 +0900

    Add multinormal (#46).

--- FsRandom/Math.fs
+++ FsRandom/Math.fs
@@ -2,12 +2,17 @@
 [<AutoOpen>]
 module internal FsRandom.Math
 
+open Microsoft.FSharp.Core.LanguagePrimitives
+
 [<Literal>]
 let pi = 3.1415926535897932384626433832795
 [<Literal>]
 let ``2pi`` = 6.283185307179586476925286766559
 [<Literal>]
 let log2pi = 1.8378770664093454835606594728112
+/// <summary>The minimum number greater than 1 (= 2^(-52)).</summary>
+[<Literal>]
+let epsilon = 2.22044604925031308084726333618e-16
 
 let inline polynomial (coefficient : float list) = fun x -> List.reduceBack (fun c acc -> c + x * acc) coefficient
 
@@ -49,3 +54,124 @@ let gamma x =
 let inline cdf p =
    let sum = List.sum p
    List.accumulate (+) p |> List.map (fun w -> w / sum)
+
+let sqrtsumsq x y =
+   if abs x > abs y then
+      let r = y / x
+      in abs x * sqrt (1.0 + r * r)
+   elif y <> 0.0 then
+      let r = x / y
+      in abs y * sqrt (1.0 + r * r)
+   else
+      0.0
+      
+[<CompilationRepresentation(CompilationRepresentationFlags.ModuleSuffix)>]
+module Vector =
+   let inline normalize vector =
+      let d = Array.fold sqrtsumsq 0.0 vector
+      Array.map (fun x -> x / d) vector
+   let inline add a b =
+      let n = Array.length a
+      Array.init n (fun i -> Array.get a i + Array.get b i)
+
+[<CompilationRepresentation(CompilationRepresentationFlags.ModuleSuffix)>]
+module Matrix =
+   let transpose matrix =
+      Array2D.init (Array2D.length2 matrix) (Array2D.length1 matrix) (fun i j -> matrix.[j, i])
+
+   let inline diag n = Array2D.init n n (fun i j -> if i = j then GenericOne else GenericZero)
+   let inline diagByVector vector =
+      let n = Array.length vector
+      Array2D.init n n (fun i j -> if i = j then vector.[i] else GenericZero)
+   let getDiag matrix = Array.init (Array2D.length1 matrix) (fun i -> matrix.[i, i])
+
+   let inline add a b =
+      let m = Array2D.length1 a
+      let n = Array2D.length2 b
+      Array2D.init m n (fun i j -> a.[i, j] + b.[i, j])
+
+   let inline multiply a b =
+      let m = Array2D.length1 a
+      let k = Array2D.length2 a
+      let n = Array2D.length2 b
+      let p = Array2D.zeroCreate m n
+      for i = 0 to m - 1 do
+         for j = 0 to n - 1 do
+            let mutable sum = 0.0
+            for t = 0 to k - 1 do
+               sum <- sum + a.[i, t] * b.[t, j]
+            p.[i, j] <- sum
+      p
+
+   let inline multiplyVector a x =
+      let m = Array2D.length1 a
+      let n = Array.length x
+      let p = Array.zeroCreate m
+      for i = 0 to m - 1 do
+         let mutable sum = 0.0
+         for t = 0 to n - 1 do
+            sum <- sum +  a.[i, t] * x.[t]
+         p.[i] <- sum
+      p
+
+   let inline isSymmetric matrix =
+      let m = Array2D.length1 matrix
+      let n = Array2D.length2 matrix
+      m = n && Seq.forall (fun i -> Seq.forall (fun j -> matrix.[i, j] = matrix.[j, i]) <| seq { 0 .. i - 1 }) <| seq { 0 .. n - 1 }
+
+   let isCovarianceMatrix (matrix:float [,]) =
+      let n = Array2D.length1 matrix
+      if Array2D.length2 matrix = n then
+         let indices k = seq { 0 .. k - 1}
+         Seq.forall (fun i -> matrix.[i, i] > 0.0 && Seq.forall (fun j -> matrix.[i, j] * matrix.[i, j] <= matrix.[i, i] * matrix.[j, j]) (indices i)) (indices n)
+      else
+         false
+
+   /// <summary>Computes eigenvalues and eigenvectors of symmetric matrix.</summary>
+   /// <seealso cref="isSymmetric" />
+   let jacobi matrix =
+      let n = Array2D.length1 matrix
+      let m = n - 1
+      let eigenvalues = Array2D.copy matrix
+      let eigenvectors = diag n
+      let findMax () =
+         seq {
+            for i = 0 to m do
+               for j = 0 to m do
+                  if i <> j then
+                     yield (i, j), abs eigenvalues.[i, j]
+         }
+         |> Seq.maxBy snd
+      let loop = ref true
+      while !loop do
+         let (p, q), max = findMax ()
+         if max < epsilon then
+            loop := false
+         else
+            let app = eigenvalues.[p, p]
+            let aqq = eigenvalues.[q, q]
+            let apq = eigenvalues.[p, q]
+            let t = 0.5 * (app - aqq)
+            let ss = 0.5 * (1.0 - abs t / sqrtsumsq apq t)  // sin^2
+            let cc = 1.0 - ss  // cos^2
+            let s = if apq * t > 0.0 then -sqrt ss else sqrt ss  // sin
+            let c = sqrt cc  // cos
+            let sc = s * c  // sin * cos
+            for i = 0 to m do
+               let api = eigenvalues.[p, i]
+               let aqi = eigenvalues.[q, i]
+               eigenvalues.[p, i] <- api * c - aqi * s
+               eigenvalues.[q, i] <- api * s + aqi * c
+            for i = 0 to m do
+               eigenvalues.[i, p] <- eigenvalues.[p, i]
+               eigenvalues.[i, q] <- eigenvalues.[q, i]
+            eigenvalues.[p, p] <- app * cc - 2.0 * apq * sc + aqq * ss
+            eigenvalues.[q, q] <- aqq * cc + 2.0 * apq * sc + app * ss
+            eigenvalues.[p, q] <- 0.0
+            eigenvalues.[q, p] <- 0.0
+            for i = 0 to m do
+               let aip = eigenvectors.[i, p]
+               let aiq = eigenvectors.[i, q]
+               eigenvectors.[i, p] <- aip * c - aiq * s
+               eigenvectors.[i, q] <- aiq * c + aip * s
+      getDiag eigenvalues, eigenvectors
--- FsRandom/Statistics.fs
+++ FsRandom/Statistics.fs
@@ -398,6 +398,20 @@ let multinomial (n, weight) =
          Array.toList result, s
       )
 
+let multinormal (mu, sigma) =
+   let n = Array.length mu
+   if Array2D.length1 sigma <> n || Array2D.length2 sigma <> n then
+      invalidArg "sigma" "Invalid size of covariance matrix."
+   elif not (Matrix.isCovarianceMatrix sigma) then
+      invalidArg "sigma" "Invalid value of covariance matrix."
+   else
+      let eigenvalues, eigenvectors = Matrix.jacobi sigma
+      let d = Array.map sqrt eigenvalues |> Matrix.diagByVector
+      let q = Matrix.multiply eigenvectors d
+      let standard = Array.randomCreate n Standard.normal
+      let transform = Matrix.multiplyVector q >> Vector.add mu
+      Random.transformBy transform standard
+
 let mix model =
    let distribution = List.map fst model |> List.toArray
    let cdf = List.map snd model |> cdf
