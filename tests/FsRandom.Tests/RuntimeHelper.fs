73a3fc0609a18840f4d5d96a7cfc3dc9e99b088a
kos59125 <mail@recyclebin.jp>
2015-04-26 21:36:25 +0900

    Fix tests

--- tests/FsRandom.Tests/RuntimeHelper.fs
+++ tests/FsRandom.Tests/RuntimeHelper.fs
@@ -1,45 +1,5 @@
 ﻿[<AutoOpen>]
 module internal FsRandom.RuntimeHelper
 
-open System
-open System.IO
-open NUnit.Framework
-open RDotNet
-
-let private engine =
-   match Microsoft.Win32.Registry.LocalMachine.OpenSubKey (@"SOFTWARE\R-core") with
-   | null -> System.ApplicationException("Registry key is not found.") |> raise
-   | rCore ->
-      let is64bit = System.Environment.Is64BitProcess
-      match rCore.OpenSubKey (if is64bit then "R64" else "R") with
-      | null -> System.ApplicationException("Registry key is not found.") |> raise
-      | r ->
-         let getString key = r.GetValue (key) :?> string
-         let (%%) dir name = System.IO.Path.Combine (dir, name) 
-         let currentVersion = System.Version (getString "Current Version")
-         let binPath = getString "InstallPath" %% "bin"
-         if currentVersion < System.Version (2, 12) then
-            binPath
-         else
-            binPath %% if is64bit then "x64" else "i386"
-   |> fun path -> System.Environment.SetEnvironmentVariable ("PATH", path)
-   let engine = REngine.CreateInstance ("REngine")
-   engine.Initialize ()
-   engine
-let getREngine () = engine
-
-let loadPackage (package:string) =
-   let makeSafePath (path:string) = path.Replace (Path.DirectorySeparatorChar, '/')
-   engine.Evaluate (System.String.Format ("""if (!require({0}, lib.loc=c(.libPaths(), "{1}"))) {{
-      install.packages("{0}", lib="{1}", repos="http://cran.r-project.org")
-      stopifnot(require({0}, lib.loc="{1}"))
-   }}""", package, makeSafePath Environment.CurrentDirectory))
-   |> ignore
-
-type S = SymbolicExpressionExtension
-let getP = function
-   | List (testResult) -> testResult.["p.value"] |> S.AsNumeric |> Seq.head
-   | _ -> failwith "not a test result object"
-
-let inline curry f x y = f (x, y)
-let inline uncurry f (x, y) = f x y
+let curry f x y = f (x, y)
+let uncurry f (x, y) = f x y
--- tests/FsRandom.Tests/StatisticsTest.fs
+++ tests/FsRandom.Tests/StatisticsTest.fs
@@ -2,321 +2,201 @@
 
 open FsRandom.Statistics
 open FsUnit
+open MathNet.Numerics
+open MathNet.Numerics.Distributions
 open NUnit.Framework
-open RDotNet
 
-let p = 0.01
 let n = 5000
 let getSamples g = Seq.ofRandom g Utility.defaultState |> Seq.take n |> Seq.toArray
 
+// Anderson-Darling test
+let adTest distribution samples =
+   let samples = Array.sort samples
+   let S =
+      let n = Array.length samples
+      let f = distribution
+      Array.init n (fun j -> ((2.0 * float j + 1.0) / float  n) * (log (f samples.[j]) + log (1.0 - f samples.[n - 1 - j])))
+      |> Array.sum
+   let A2 = -float samples.Length - S
+   let CV = 3.857  // both mean and variance are known
+   A2 < CV
+
+// Chi-square goodness of fit test
+let chisqTest distribution samples =
+   let n = Array.length samples
+   let f = distribution
+   let samples = Seq.countBy id samples |> Seq.toArray |> Array.sortBy fst
+   let chi2 =
+      let sumP = samples |> Array.sumBy (fst >> f)
+      let residual = 1.0 - sumP
+      let sampled =
+         samples
+         |> Array.map (fun (k, obs) ->
+            let npi = float n * f k
+            pown (float obs - npi) 2 / npi
+         )
+         |> Array.sum
+      if residual > 1.0e-6 then
+         sampled + float n * residual
+      else
+         sampled
+   chi2 < SpecialFunctions.GammaLowerRegularizedInv (float (n - 1) / 2.0, 0.99)
+
 [<TearDown>]
 let tearDown () =
-   let engine = getREngine ()
-   engine.Evaluate ("""rm(list=ls())""") |> ignore
+   ()
 
 [<Test>]
 let ``Validates uniform`` () =
-   let engine = getREngine ()
-   use samples =
-      let samples = getSamples (uniform (-10.0, 10.0))
-      engine.CreateNumericVector (samples)
-   engine.SetSymbol ("x", samples)
-   engine.Evaluate ("""ks.test(x, "punif", min=-10, max=10)""")
-   |> getP
-   |> should be (greaterThan p)
+   let distribution = ContinuousUniform (-10.0, 10.0)
+   getSamples (uniform (-10.0, 10.0))
+   |> adTest distribution.CumulativeDistribution
+   |> should be True
 
 [<Test>]
 let ``Validates loguniform`` () =
-   let engine = getREngine ()
-   use samples =
-      let samples = getSamples (loguniform (1.0, 100.0))
-      engine.CreateNumericVector (samples)
-   engine.SetSymbol ("x", samples)
-   use plogunif = engine.Evaluate ("""plogunif <- function(x, min, max) {
-      if (x < min) {
-         0
-      } else if (min <= x && x <= max) {
-         log(x) / (log(max) - log(min))
-      } else {
-         1
-      }
-   }""")
-   engine.Evaluate ("""ks.test(x, "plogunif", min=1, max=100)""")
-   |> getP
-   |> should be (greaterThan p)
+   let cdf (a, b) x = 1.0 / (log b - log a) * log x
+   getSamples (loguniform (1.0, 100.0))
+   |> adTest (cdf (1.0, 100.0))
+   |> should be True
 
 [<Test>]
 let ``Validates triangular`` () =
-   let engine = getREngine ()
-   use samples =
-      let samples = getSamples (triangular (-3.3, 10.7, 2.1))
-      engine.CreateNumericVector (samples)
-   engine.SetSymbol ("x", samples)
-   loadPackage "triangle"
-   engine.Evaluate ("""ks.test(x, "ptriangle", a=-3.3, b=10.7, c=2.1)""")
-   |> getP
-   |> should be (greaterThan p)
+   let distribution = Triangular (-3.3, 10.7, 2.1)
+   getSamples (triangular (-3.3, 10.7, 2.1))
+   |> adTest distribution.CumulativeDistribution
+   |> should be True
 
 [<Test>]
 let ``Validates normal`` () =
-   let engine = getREngine ()
-   use samples =
-      let samples = getSamples (normal (-5.0, 3.0))
-      engine.CreateNumericVector (samples)
-   engine.SetSymbol ("x", samples)
-   engine.Evaluate ("""ks.test(x, "pnorm", mean=-5, sd=3)""")
-   |> getP
-   |> should be (greaterThan p)
+   let distribution = Normal (-5.0, 3.0)
+   getSamples (normal (-5.0, 3.0))
+   |> adTest distribution.CumulativeDistribution
+   |> should be True
 
 [<Test>]
 let ``Validates lognormal`` () =
-   let engine = getREngine ()
-   use samples =
-      let samples = getSamples (lognormal (3.1, 7.2))
-      engine.CreateNumericVector (samples)
-   engine.SetSymbol ("x", samples)
-   engine.Evaluate ("""ks.test(x, "plnorm", meanlog=3.1, sdlog=7.2)""")
-   |> getP
-   |> should be (greaterThan p)
+   let distribution = LogNormal (3.1, 7.2)
+   getSamples (lognormal (3.1, 7.2))
+   |> adTest distribution.CumulativeDistribution
+   |> should be True
 
 [<Test>]
 let ``Validates gamma (shape < 1)`` () =
-   let engine = getREngine ()
-   use samples =
-      let samples = getSamples (gamma (0.3, 2.0))
-      engine.CreateNumericVector (samples)
-   engine.SetSymbol ("x", samples)
-   engine.Evaluate ("""ks.test(x, "pgamma", shape=0.3, scale=2)""")
-   |> getP
-   |> should be (greaterThan p)
+   let distribution = Gamma (0.3, 1.0 / 2.0)
+   getSamples (gamma (0.3, 2.0))
+   |> adTest distribution.CumulativeDistribution
+   |> should be True
 
 [<Test>]
 let ``Validates gamma (shape > 1)`` () =
-   let engine = getREngine ()
-   use samples =
-      let samples = getSamples (gamma (5.6, 0.4))
-      engine.CreateNumericVector (samples)
-   engine.SetSymbol ("x", samples)
-   engine.Evaluate ("""ks.test(x, "pgamma", shape=5.6, scale=0.4)""")
-   |> getP
-   |> should be (greaterThan p)
+   let distribution = Gamma (5.6, 1.0 / 0.4)
+   getSamples (gamma (5.6, 0.4))
+   |> adTest distribution.CumulativeDistribution
+   |> should be True
 
 [<Test>]
 let ``Validates gamma (shape is integer)`` () =
-   let engine = getREngine ()
-   use samples =
-      let samples = getSamples (gamma (3.0, 7.9))
-      engine.CreateNumericVector (samples)
-   engine.SetSymbol ("x", samples)
-   engine.Evaluate ("""ks.test(x, "pgamma", shape=3, scale=7.9)""")
-   |> getP
-   |> should be (greaterThan p)
+   let distribution = Gamma (3.0, 1.0 / 7.9)
+   getSamples (gamma (3.0, 7.9))
+   |> adTest distribution.CumulativeDistribution
+   |> should be True
 
 [<Test>]
 let ``Validates exponential`` () =
-   let engine = getREngine ()
-   use samples =
-      let samples = getSamples (exponential (1.5))
-      engine.CreateNumericVector (samples)
-   engine.SetSymbol ("x", samples)
-   engine.Evaluate ("""ks.test(x, "pexp", rate=1.5)""")
-   |> getP
-   |> should be (greaterThan p)
+   let distribution = Exponential (1.5)
+   getSamples (exponential (1.5))
+   |> adTest distribution.CumulativeDistribution
+   |> should be True
 
 [<Test>]
 let ``Validates weibull`` () =
-   let engine = getREngine ()
-   use samples =
-      let samples = getSamples (weibull (6.1, 1.4))
-      engine.CreateNumericVector (samples)
-   engine.SetSymbol ("x", samples)
-   engine.Evaluate ("""ks.test(x, "pweibull", shape=6.1, scale=1.4)""")
-   |> getP
-   |> should be (greaterThan p)
+   let distribution = Weibull (6.1, 1.4)
+   getSamples (weibull (6.1, 1.4))
+   |> adTest distribution.CumulativeDistribution
+   |> should be True
 
 [<Test>]
 let ``Validates gumbel`` () =
-   let engine = getREngine ()
-   use samples =
-      let samples = getSamples (gumbel (-3.0, 2.3))
-      engine.CreateNumericVector (samples)
-   engine.SetSymbol ("x", samples)
-   use pgumbel = engine.Evaluate ("""pgumbel <- function(x, mu, eta) exp(-exp(-(x - mu) / eta))""")
-   engine.Evaluate ("""ks.test(x, "pgumbel", mu=-3, eta=2.3)""")
-   |> getP
-   |> should be (greaterThan p)
+   let cdf (mu, beta) x = exp <| -exp (-(x - mu) / beta)
+   getSamples (gumbel (6.1, 1.4))
+   |> adTest (cdf (6.1, 1.4))
+   |> should be True
 
 [<Test>]
 let ``Validates beta`` () =
-   let engine = getREngine ()
-   use samples =
-      let samples = getSamples (beta (1.5, 0.4))
-      engine.CreateNumericVector (samples)
-   engine.SetSymbol ("x", samples)
-   engine.Evaluate ("""ks.test(x, "pbeta", shape1=1.5, shape2=0.4)""")
-   |> getP
-   |> should be (greaterThan p)
+   let distribution = Beta (1.5, 0.4)
+   getSamples (beta (1.5, 0.4))
+   |> adTest distribution.CumulativeDistribution
+   |> should be True
 
 [<Test>]
 let ``Validates cauchy`` () =
-   let engine = getREngine ()
-   use samples =
-      let samples = getSamples (cauchy (-1.5, 0.1))
-      engine.CreateNumericVector (samples)
-   engine.SetSymbol ("x", samples)
-   engine.Evaluate ("""ks.test(x, "pcauchy", location=-1.5, scale=0.1)""")
-   |> getP
-   |> should be (greaterThan p)
+   let distribution = Cauchy (-1.5, 0.1)
+   getSamples (cauchy (-1.5, 0.1))
+   |> adTest distribution.CumulativeDistribution
+   |> should be True
 
 [<Test>]
 let ``Validates chisquare`` () =
-   let engine = getREngine ()
-   use samples =
-      let samples = getSamples (chisquare (10))
-      engine.CreateNumericVector (samples)
-   engine.SetSymbol ("x", samples)
-   engine.Evaluate ("""ks.test(x, "pchisq", df=10)""")
-   |> getP
-   |> should be (greaterThan p)
+   let distribution = ChiSquared (10.0)
+   getSamples (chisquare (10))
+   |> adTest distribution.CumulativeDistribution
+   |> should be True
 
 [<Test>]
 let ``Validates studentT`` () =
-   let engine = getREngine ()
-   use samples =
-      let samples = getSamples (studentT (3))
-      engine.CreateNumericVector (samples)
-   engine.SetSymbol ("x", samples)
-   engine.Evaluate ("""ks.test(x, "pt", df=3)""")
-   |> getP
-   |> should be (greaterThan p)
+   let distribution = StudentT (0.0, 1.0, 3.0)
+   getSamples (studentT (3))
+   |> adTest distribution.CumulativeDistribution
+   |> should be True
 
-[<Test>]
-let ``Validates vonMises`` () =
-   let engine = getREngine ()
-   use samples =
-      let samples = getSamples (vonMises (1.0, 2.0))
-      engine.CreateNumericVector (samples)
-   engine.SetSymbol ("x", samples)
-   engine.Evaluate ("""
-   dvonMises <- function(x, mu, kappa) {
-      if (x < -pi || pi < x) {
-         0
-      } else {
-         i <- integrate(function(t) exp(kappa * cos(t)), 0, pi)$value
-         exp(kappa * cos(x - mu)) / (2 * i)
-      }
-   }
-   pvonMises <- function(x, mu, kappa) {
-      integrate(dvonMises, -pi, x, mu=mu, kappa=kappa)$value
-   }
-   pvonMises <- Vectorize(pvonMises, "x")
-   ks.test(x, "pvonMises", mu=1, kappa=2)""")
-   |> getP
-   |> should be (greaterThan p)
+// CDF is unknown
+//[<Test>]
+//let ``Validates vonMises`` () =
+//   ()
 
 [<Test>]
 let ``Validates uniformDiscrete`` () =
-   let engine = getREngine ()
-   use samples =
-      let samples = getSamples (uniformDiscrete (-10, 10))
-      engine.CreateIntegerVector (samples)
-   engine.SetSymbol ("x", samples)
-   engine.Evaluate ("""
-   x <- table(x)
-   p <- rep(1/21, 21L)
-   if (length(x) == 21L) {
-      chisq.test(x, p=p)
-   } else {
-      chisq.test(c(x, rep(0L, 21L - length(x))), p=p)
-   }""")
-   |> getP
-   |> should be (greaterThan p)
+   let distribution = DiscreteUniform (-10, 10)
+   getSamples (uniformDiscrete (-10, 10))
+   |> chisqTest distribution.Probability
+   |> should be True
 
 [<Test>]
 let ``Validates poisson`` () =
-   let engine = getREngine ()
-   use samples =
-      let samples = getSamples (poisson (5.2))
-      engine.CreateIntegerVector (samples)
-   engine.SetSymbol ("x", samples)
-   engine.Evaluate ("""
-   x <- table(x)
-   p <- dpois(as.integer(names(x)), 5.2)
-   chisq.test(c(x, 0L), p=c(p, 1 - sum(p)))""")
-   |> getP
-   |> should be (greaterThan p)
+   let distribution = Poisson (5.2)
+   getSamples (poisson (5.2))
+   |> chisqTest distribution.Probability
+   |> should be True
 
 [<Test>]
 let ``Validates geometric0`` () =
-   let engine = getREngine ()
-   use samples =
-      let samples = getSamples (geometric0 (0.2))
-      engine.CreateIntegerVector (samples)
-   engine.SetSymbol ("x", samples)
-   engine.Evaluate ("""
-   x <- table(x)
-   p <- dgeom(as.integer(names(x)), 0.2)
-   if (abs(1 - sum(p)) > .Machine$double.eps) {
-      chisq.test(c(x, 0L), p=c(p, 1 - sum(p)))
-   } else {
-      chisq.test(x, p=p)
-   }""")
-   |> getP
-   |> should be (greaterThan p)
+   let distribution = Geometric (0.2)
+   getSamples (geometric0 (0.2))
+   |> chisqTest (fun x -> distribution.Probability (x + 1))  // Math.NET Numerics' Geometric supports [1, 2, ...]
+   |> should be True
 
 [<Test>]
 let ``Validates geometric1`` () =
-   let engine = getREngine ()
-   use samples =
-      let samples = getSamples (geometric1 (0.7))
-      engine.CreateIntegerVector (samples)
-   engine.SetSymbol ("x", samples)
-   engine.Evaluate ("""
-   x <- table(x)
-   p <- dgeom(as.integer(names(x)) - 1L, 0.7)
-   if (abs(1 - sum(p)) > .Machine$double.eps) {
-      chisq.test(c(x, 0L), p=c(p, 1 - sum(p)))
-   } else {
-      chisq.test(x, p=p)
-   }""")
-   |> getP
-   |> should be (greaterThan p)
+   let distribution = Geometric (0.7)
+   getSamples (geometric1 (0.7))
+   |> chisqTest distribution.Probability
+   |> should be True
 
 [<Test>]
 let ``Validates bernoulli`` () =
-   let engine = getREngine ()
-   use samples =
-      let samples = getSamples (bernoulli (0.7))
-      engine.CreateIntegerVector (samples)
-   engine.SetSymbol ("x", samples)
-   engine.Evaluate ("""
-   x <- table(x)
-   p <- dbinom(as.integer(names(x)), 1L, 0.7)
-   if (length(x) == 2L) {
-      chisq.test(x, p=p)
-   } else {
-      chisq.test(c(x, 0L), p=c(p, 1 - sum(p)))
-   }""")
-   |> getP
-   |> should be (greaterThan p)
+   let distribution = Bernoulli (0.4)
+   getSamples (bernoulli (0.4))
+   |> chisqTest distribution.Probability
+   |> should be True
 
 [<Test>]
 let ``Validates binomial`` () =
-   let engine = getREngine ()
-   use samples =
-      let samples = getSamples (binomial (20, 0.3))
-      engine.CreateIntegerVector (samples)
-   engine.SetSymbol ("x", samples)
-   engine.Evaluate ("""
-   x <- table(x)
-   p <- dbinom(as.integer(names(x)), 20L, 0.3)
-   if (length(x) == 21L) {
-      chisq.test(x, p=p)
-   } else {
-      chisq.test(c(x, 0L), p=c(p, 1 - sum(p)))
-   }""")
-   |> getP
-   |> should be (greaterThan p)
+   let distribution = Binomial (0.3, 20)
+   getSamples (binomial (20, 0.3))
+   |> chisqTest distribution.Probability
+   |> should be True
 
 // TODO: implement
 //[<Test>]
@@ -354,16 +234,16 @@ let ``wishart returns positive and positive semidefinite matrices`` () =
 
 [<Test>]
 let ``Validates mix (float)`` () =
-   let engine = getREngine ()
-   use samples =
-      let samples = getSamples (mix [gamma (3.0, 2.0), 1.0; normal (-2.0, 1.0), 3.0])
-      engine.CreateNumericVector (samples)
-   engine.SetSymbol ("x", samples)
-   engine.Evaluate ("""
-   pmix <- function(x) 0.25 * pgamma(x, shape=3, scale=2) + 0.75 * pnorm(x, mean=-2, sd=1)
-   ks.test(x, "pmix")""")
-   |> getP
-   |> should be (greaterThan p)
+   let cdf x =
+      let gamma = Gamma (3.0, 1.0 / 2.0)
+      let normal = Normal (-2.0, 1.0)
+      if x <= 0.0 then
+         0.75 * normal.CumulativeDistribution (x)
+      else
+         0.25 * gamma.CumulativeDistribution (x) + 0.75 * normal.CumulativeDistribution (x)
+   getSamples (mix [gamma (3.0, 2.0), 1.0; normal (-2.0, 1.0), 3.0])
+   |> adTest cdf
+   |> should be True
 
 [<Test>]
 let ``uniformDiscrete on full-range of int generates both positive and negative values`` () =
